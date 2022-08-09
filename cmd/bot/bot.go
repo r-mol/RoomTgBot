1108fd5fb95251aa23f640dbb148ab7c7b5d8cd3
Roman Molochkov <molochkov2017@gmail.com>
2022-08-09 11:37:24 +0300

    feat: remove handling from file

---
+++ cmd/bot/bot.go
@@ -0,0 +1,29 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	telegram "gopkg.in/telebot.v3"
+)
+
+func Setup() {
+	pref := telegram.Settings{
+		Token:  "5455937729:AAEVDvLDJczTncZ0aOfIA0Xn6dVcFgcMIO0",
+		Poller: &telegram.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	fmt.Println("5455937729:AAEVDvLDJczTncZ0aOfIA0Xn6dVcFgcMIO0")
+
+	bot, err := telegram.NewBot(pref)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	startHandling(bot)
+
+	bot.Start()
+}
