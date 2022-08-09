616c4227acc9df0ae1e56b8aafb5cce746104a6a
Roman Molochkov <molochkov2017@gmail.com>
2022-08-09 11:36:54 +0300

    feat: create file for handling

---
+++ cmd/bot/handler.go
@@ -0,0 +1,33 @@
+package bot
+
+import (
+	"RoomTgBot/internal"
+	telegram "gopkg.in/telebot.v3"
+)
+
+func startHandling(bot *telegram.Bot) {
+	bot.Handle(commandStart, func(ctx telegram.Context) error {
+		newUser := &internal.User{
+			ID:        ctx.Sender().ID,
+			FirstName: ctx.Sender().FirstName,
+			Username:  ctx.Sender().Username,
+			IsBot:     ctx.Sender().IsBot}
+
+		if newUser.IsBot {
+			defer bot.Stop()
+			return ctx.Send("You are fucking bot...")
+		}
+
+		// Add to new user to database
+
+		return ctx.Send("Nice to meet you " + newUser.FirstName + "!!!")
+	})
+
+	bot.Handle(commandHelp, func(ctx telegram.Context) error {
+		return ctx.Send("Help command handler")
+	})
+
+	bot.Handle(telegram.OnText, func(ctx telegram.Context) error {
+		return ctx.Send(ctx.Message().Text)
+	})
+}
