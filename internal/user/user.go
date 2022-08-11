cdc7ede669a0aef4014d333a5128d718ba2b148c
Roman Molochkov <molochkov2017@gmail.com>
2022-08-11 07:16:33 +0300

    refactor: change type of variable CurState of User

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,25 +1,27 @@
 package user
 
 import (
+	"RoomTgBot/internal/state"
 	telegram "gopkg.in/telebot.v3"
 )
 
 type User struct {
 	ID int64 `json:"id"`
 
-	FirstName string `json:"first_name"`
-	Username  string `json:"username"`
-	IsBot     bool   `json:"is_bot"`
-	CurState  string `json:"cur_state"`
+	FirstName string       `json:"first_name"`
+	Username  string       `json:"username"`
+	IsBot     bool         `json:"is_bot"`
+	CurState  *state.State `json:"cur_state"`
 }
 
 func CreateUser(bot *telegram.Bot, ctx telegram.Context, newUser *User) error {
 	*newUser = User{
-		ID:        ctx.Sender().ID,
+		ID: ctx.Sender().ID,
+
 		FirstName: ctx.Sender().FirstName,
 		Username:  ctx.Sender().Username,
 		IsBot:     ctx.Sender().IsBot,
-		CurState:  " ",
+		CurState:  &state.State{},
 	}
 
 	if newUser.IsBot {
