21b3a2d47754a85d4901af4f93e0f4cff9239d42
Roman Molochkov <molochkov2017@gmail.com>
2022-08-11 06:41:30 +0300

    feat: replace constants of commands to separate file

---
+++ internal/commands/commands.go
@@ -0,0 +1,19 @@
+package commands
+
+const (
+	CommandStart      = "/start"
+	CommandStop       = "/stop"
+	CommandHelp       = "/help"
+	CommandBringWater = "/bring_water"
+	CommandClean      = "/clean_room"
+
+	CommandRoom     = "\U0001F6D6 Room"
+	CommandNews     = "📰 News"
+	CommandExam     = "🧐 Exam"
+	CommandSettings = "⚙️ Settings"
+	CommandBack     = "↩️ Back"
+
+	CommandShop     = "🛍 Shop"
+	CommandAquaMan  = "\U0001F9A6 Aqua-Man"
+	CommandCleanMan = "\U0001F9F9 Clean-Man"
+)
