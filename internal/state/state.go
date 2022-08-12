47d0f2238d29978e5ccda38667688a07954ce39a
Roman Molochkov <molochkov2017@gmail.com>
2022-08-12 11:59:58 +0300

    refactor: create function of reset all to Zero state

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,12 +1,15 @@
 package state
 
 import (
+	"RoomTgBot/internal/commands"
 	"RoomTgBot/internal/menus"
 
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/go-redis/redis/v9"
 	telegram "gopkg.in/telebot.v3"
@@ -20,74 +23,126 @@ type State struct {
 	IsNow     bool      `json:"is_now"`
 }
 
-func CheckOfUserState(contex context.Context, rdb *redis.Client, ctx telegram.Context, prevCommand, initCommand string) (*State, error) {
-	prevState := &State{}
+type States map[string]*State
+
+const InitState = "init_state"
+
+func CheckOfUserState(contex context.Context, rdb *redis.Client, ctx telegram.Context, prevCommand, initCommand string) error {
+	states := States{}
 	curState := &State{}
 
-	err := GetStateFromRDB(contex, rdb, prevState, prevCommand)
+	err := GetStatesFromRDB(contex, rdb, ctx, &states)
+
+	if err == redis.Nil {
+		return ctx.Send("Please restart bot ✨")
+	} else if err != nil {
+		return err
+	}
+
+	prevState, ok := states[prevCommand]
+	if !ok {
+		err = ctx.Send("Something bad happened, we return you to the beginning", menus.MainMenu)
+
+		if err != nil {
+			return err
+		}
+
+		err = resetToZeroState(contex, rdb, ctx)
+
+		if err == redis.Nil {
+			return ctx.Send("Please restart bot ✨")
+		} else if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("bad request prevState is not exist")
+	}
 
 	log.Println(prevState)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	if !prevState.IsNow {
-		return nil, ctx.Send("Something bad happened, we return you to the beginning", menus.MainMenu)
+		err = ctx.Send("Something bad happened, we return you to the beginning", menus.MainMenu)
+
+		if err != nil {
+			return err
+		}
+
+		err = resetToZeroState(contex, rdb, ctx)
+
+		if err == redis.Nil {
+			return ctx.Send("Please restart bot ✨")
+		} else if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("bad request: prevState is not now")
 	}
 
 	prevState.IsNow = false
-	err = SetStateToRDB(contex, rdb, prevState)
+	states[prevCommand] = prevState
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	err = GetStateFromRDB(contex, rdb, curState, initCommand)
+	curState, ok = states[initCommand]
 
-	switch err {
-	case redis.Nil:
+	if !ok {
 		curState = &State{
 			InitState: initCommand,
 			PrevState: prevState.InitState,
 			IsNow:     true,
 		}
-
-		err = SetStateToRDB(contex, rdb, curState)
-
-		if err != nil {
-			return nil, err
-		}
-	default:
+	} else {
 		curState.IsNow = true
-		err = SetStateToRDB(contex, rdb, curState)
+	}
 
-		if err != nil {
-			return nil, err
-		}
+	states[initCommand] = curState
+	states[InitState] = curState
 
-		log.Println(prevState)
-		log.Println(curState)
+	err = SetStatesToRDB(contex, rdb, ctx, &states)
 
-		return curState, err
+	if err != nil {
+		return err
 	}
 
 	log.Println(prevState)
 	log.Println(curState)
 
+	return err
+}
+
+func GetCurStateFromRDB(contex context.Context, rdb *redis.Client, ctx telegram.Context) (*State, error) {
+	states := States{}
+	err := GetStatesFromRDB(contex, rdb, ctx, &states)
+
+	if err == redis.Nil {
+		return nil, ctx.Send("Please restart bot ✨")
+	} else if err != nil {
+		return nil, err
+	}
+
+	curState := states[InitState]
+
 	return curState, nil
 }
 
-func GetStateFromRDB(contex context.Context, rdb *redis.Client, st *State, command string) error {
-	stBytes, err := rdb.Get(contex, command).Result()
+func GetStatesFromRDB(contex context.Context, rdb *redis.Client, ctx telegram.Context, sts *States) error {
+	id := strconv.FormatInt(ctx.Sender().ID, 10)
+
+	stateBytes, err := rdb.Get(contex, id).Result()
 
 	switch {
 	case err == redis.Nil:
-		return err
+		sts = &States{}
 	case err != nil:
 		return err
 	default:
-		err = json.Unmarshal([]byte(stBytes), &st)
+		err = json.Unmarshal([]byte(stateBytes), &sts)
 		if err != nil {
 			return err
 		}
@@ -96,15 +151,49 @@ func GetStateFromRDB(contex context.Context, rdb *redis.Client, st *State, comma
 	return nil
 }
 
-func SetStateToRDB(contex context.Context, rdb *redis.Client, st *State) error {
-	stateBytes, err := json.Marshal(st)
+func SetStatesToRDB(contex context.Context, rdb *redis.Client, ctx telegram.Context, sts *States) error {
+	id := strconv.FormatInt(ctx.Sender().ID, 10)
+
+	stateBytes, err := json.Marshal(sts)
 
 	if err != nil {
 		return err
 	}
 
-	err = rdb.Set(contex, st.InitState, stateBytes, 0).Err()
+	err = rdb.Set(contex, id, stateBytes, 0).Err()
 	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resetToZeroState(contex context.Context, rdb *redis.Client, ctx telegram.Context) error {
+	states := States{}
+	curState, err := GetCurStateFromRDB(contex, rdb, ctx)
+
+	if err == redis.Nil {
+		return ctx.Send("Please restart bot ✨")
+	} else if err != nil {
+		return err
+	}
+
+	curState.IsNow = false
+
+	states[curState.InitState] = curState
+
+	newCurState := states[commands.CommandStart]
+
+	newCurState.IsNow = true
+
+	states[commands.CommandStart] = newCurState
+	states[InitState] = newCurState
+
+	err = SetStatesToRDB(contex, rdb, ctx, &states)
+
+	if err == redis.Nil {
+		return ctx.Send("Please restart bot ✨")
+	} else if err != nil {
 		return err
 	}
 
