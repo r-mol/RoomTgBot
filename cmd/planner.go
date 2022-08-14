bf9e2a825a31316119057c5bd43309dbc2596859
Demezy <38487319+Demezy@users.noreply.github.com>
2022-08-14 19:37:24 +0300

    feat: add RunOnce function

--- cmd/planner.go
+++ cmd/planner.go
@@ -11,4 +11,11 @@ var Cron = cron.New()
 func ScheduleForEvery(t time.Duration, cmd func()) {
 	Cron.Schedule(cron.Every(t), cron.FuncJob(cmd))
 }
-
+func RunOnceAfter(t time.Duration, cmd func()) {
+	// Library has no human way to do it
+	// "Fine. I'll do it myself." (didn't get => Youtube it)
+	go func() {
+		time.Sleep(t)
+		cmd()
+	}()
+}
