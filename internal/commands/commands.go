347ae933c8a91d5247b744abadcb55a2536acc31
Roman Molochkov <molochkov2017@gmail.com>
2022-08-16 19:42:05 +0300

    refactor: Arrange all in menus and commands

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,34 +1,46 @@
 package commands
 
 const (
+	// Commands
 	CommandStart      = "/start"
 	CommandContact    = "/contact"
 	CommandHelp       = "/help"
 	CommandBringWater = "/bring_water"
 	CommandCleanRoom  = "/clean_room"
 
+	// Main commands
 	CommandRoom     = "\U0001F6D6 Room"
 	CommandNews     = "📰 News"
 	CommandExam     = "🧐 Exam"
 	CommandSettings = "⚙️ Settings"
 	CommandBack     = "Back ↩️"
 
+	// List commands
+	CommandPrevious = "⬅️"
+	CommandNext     = "➡️"
+	CommandExit     = "Exit"
+
+	// Room commands
 	CommandShop     = "🛍 Shop"
 	CommandAquaMan  = "\U0001F9A6 Aqua-Man"
 	CommandCleanMan = "\U0001F9F9 Clean-Man"
 
+	// Upload purchase commands
 	CommandPostPurchase   = "📦 Post purchase"
 	CommandUploadPurchase = "📥 Upload purchase"
 	CommandPurchaseDone   = "✅ Purchase done"
 	CommandCheck          = "\U0001F9FE Check"
 
+	// News commands
 	CommandNewsDone    = "✅ News done"
 	CommandPostNews    = "✉️ Post news"
 	CommandDeleteDraft = "🗑 Remove draft"
 
+	// Settings commands
 	CommandNotificationSettings = "📬 Notification settings"
 	CommandSettingsOfBot        = "🔐 Settings of bot"
 
+	// Exam commands
 	CommandUploadExam = "📥 Upload exam"
 	CommandGetExam    = "📤 Get exam"
 	CommandExamDone   = "✅ Exam done"
