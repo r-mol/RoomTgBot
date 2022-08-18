b7dc5c86e1a354acf5fa863fc28874f4aac166cd
Roman Molochkov <molochkov2017@gmail.com>
2022-08-18 15:48:25 +0300

    feat: restore crom example

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,44 @@ import (
 	"RoomTgBot/cmd/bot"
 
 	"fmt"
+	"time"
 )
+
+func init() {
+	Cron.Start()
+}
 
 func main() {
 	fmt.Println("Hello Goland")
 	testAllServices()
 }
 
+func cronExample() {
+	// set repeating action
+	const sec = time.Second
+	id := ScheduleForEvery(1*sec/2, func() { fmt.Println("repeating") })
+	// set one time action. Cannot be canceled (for now?)
+	RunOnceAfter(sec/2*3, func() { fmt.Println("Once") })
+	// cancel first task
+	RunOnceAfter(sec*2, func() {
+		fmt.Println("cancel task")
+		CancelTask(id)
+	})
+
+	// because tasks execute in background
+	time.Sleep(3 * time.Second)
+}
+
 func testAllServices() {
+	// cron
+	cronExample()
+
+	// mongodb
+	//DBTest()
+
+	// redis
+	//ExampleClient()
+
+	// start echo bot
 	bot.Setup()
 }
