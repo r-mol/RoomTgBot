94a4ed3ddca02321e602259d106035610e26da82
Alexandr Ragulin <klodiys62@gmail.com>
2022-08-27 10:59:46 +0300

    feat: add functions to get next user in order

--- internal/user/user.go
+++ internal/user/user.go
@@ -2,11 +2,16 @@ package user
 
 import (
 	"RoomTgBot/internal/consts"
+	"RoomTgBot/internal/mongodb"
+	"RoomTgBot/internal/types"
 	"context"
 	"encoding/json"
+	"fmt"
+	"sort"
 	"strconv"
 
 	"github.com/go-redis/redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
 	telegram "gopkg.in/telebot.v3"
 )
 
@@ -15,7 +20,7 @@ func CreateUser(contex context.Context, rdb *redis.Client, bot *telegram.Bot, ct
 
 	_, err := rdb.Get(contex, idString).Result()
 	if err == redis.Nil {
-		// TODO add new user to database
+		// TODO add new user to mongoDB and don't forget to normalizeOrder
 		err = SetUserToRDB(contex, rdb, ctx)
 		if err != nil {
 			return err
@@ -27,6 +32,63 @@ func CreateUser(contex context.Context, rdb *redis.Client, bot *telegram.Bot, ct
 		}
 	} else if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// Normalize order of people so the smallest one is 0 and others ared 1, 2...
+func normalizeOrder(users []types.User) []types.User {
+	sort.Slice(users, func(p, q int) bool {
+		return users[p].Order < users[q].Order
+	})
+
+	for index := range users {
+		users[index].Order = uint(index)
+	}
+
+	return users
+}
+
+// Users list and map should be normalized using normalizeOrder
+func NextInOrder(prevID int64, usersMap map[int64]types.User, users []types.User) (int64, error) {
+	prevOrder := usersMap[prevID].Order
+
+	if len(users) == 0 {
+		return 0, fmt.Errorf("no next user, because list of users is empty")
+	}
+
+	index := (int(prevOrder) + 1) % len(users)
+
+	return users[index].TelegramID, nil
+}
+
+func MongoGetMap(ctx context.Context, client *mongo.Client) (map[int64]types.User, error) {
+	mongoUsers, err := mongodb.GetAll[types.User](ctx, client, consts.MongoUsersCollection)
+	if err != nil {
+		return map[int64]types.User{}, fmt.Errorf("unable to get Users from mongodb: %v", err)
+	}
+
+	users := map[int64]types.User{}
+	for _, elem := range mongoUsers {
+		users[elem.TelegramID] = elem
+	}
+
+	return users, nil
+}
+func MongoGet(ctx context.Context, client *mongo.Client) ([]types.User, error) {
+	mongoUsers, err := mongodb.GetAll[types.User](ctx, client, "Users")
+	if err != nil {
+		return []types.User{}, fmt.Errorf("unable to get Users from mongodb: %v", err)
+	}
+
+	return mongoUsers, nil
+}
+func MongoAdd(ctx context.Context, client *mongo.Client, user *types.User) error {
+	_, err := mongodb.AddOne(ctx, client, consts.MongoUsersCollection, user)
+
+	if err != nil {
+		return fmt.Errorf("unable to add user to mongodb: %v", err)
 	}
 
 	return nil
