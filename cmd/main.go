e37d1206913f17e4ac996b6495f1c3d226983493
Demezy <38487319+Demezy@users.noreply.github.com>
2022-09-10 11:48:32 +0300

    refactor: clean main from examples

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"RoomTgBot/cmd/bot"
 
 	"fmt"
-	"time"
+	// "time"
 )
 
 func init() {
@@ -13,40 +13,5 @@ func init() {
 
 func main() {
 	fmt.Println("Hello Goland")
-	testAllServices()
-}
-
-const (
-	two   = 2
-	three = 3
-)
-
-func cronExample() {
-	// set repeating action
-	const sec = time.Second
-	id := ScheduleForEvery(sec/two, func() { fmt.Println("repeating") })
-	// set one time action. Cannot be canceled (for now?)
-	RunOnceAfter(sec/two*three, func() { fmt.Println("Once") })
-	// cancel first task
-	RunOnceAfter(sec*two, func() {
-		fmt.Println("cancel task")
-		CancelTask(id)
-	})
-
-	// because tasks execute in background
-	time.Sleep(three * time.Second)
-}
-
-func testAllServices() {
-	// cron
-	//cronExample()
-
-	// mongodb
-	//DBTest()
-
-	// redis
-	//ExampleClient()
-
-	// start echo bot
 	bot.Setup()
 }
